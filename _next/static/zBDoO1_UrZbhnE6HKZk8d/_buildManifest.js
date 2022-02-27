ca1234ce9c952a2d27cfa61c447b69b181991145
fred <you@example.com>
2022-02-26 21:28:01 -0500

    Deploy gh-pages

---
+++ _next/static/zBDoO1_UrZbhnE6HKZk8d/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/361-492617a94107fc74.js","static/chunks/pages/index-617599502ec4d526.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
